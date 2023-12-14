84427123d8a4f6d05106d4539c8b90ac0a1ac8db
R3call <thenoobyzombie@gmail.com>
2023-12-14 04:02:03 +0100

    added: new api link

--- utils/sendPrompt.ts
+++ utils/sendPrompt.ts
@@ -1,7 +1,44 @@
-import { PromptResponse } from "@/lib/types";
+import { AIResponseHistory, PromptResponse } from "@/lib/types";
 import axios from "axios";
 
-export async function sendPrompt(prompt: string):Promise<PromptResponse> {
-    const res = await axios.post(`api/sendPrompt?message=${prompt}`);
-    return res.data;
+export async function sendPrompt(prompt: string): Promise<PromptResponse> {
+    try {
+        const res = await axios.post(`api/sendPrompt?message=${prompt}`);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Error in sendPrompt API call:", error.response?.data || error.message);
+        } else {
+            console.error("Non-Axios error in sendPrompt:", error);
+        }
+        throw error; 
+    }
+}
+
+export async function sendQuestionAnswers(prompt: string): Promise<PromptResponse> {
+    try {
+        const res = await axios.post(`api/checkAnswers?answers=${prompt}`);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Error in sendPrompt API call:", error.response?.data || error.message);
+        } else {
+            console.error("Non-Axios error in sendPrompt:", error);
+        }
+        throw error; 
+    }
+}
+
+export async function getAIHistory(): Promise<AIResponseHistory> {
+    try {
+        const res = await axios.get(`api/getAIHistory`);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.error("Error in getAIHistory API call:", error.response?.data || error.message);
+        } else {
+            console.error("Non-Axios error in getAIHistory:", error);
+        }
+        throw error; 
+    }
 }
