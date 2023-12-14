5239d1d36b45619abe690503ae2b5c365b920881
R3call <thenoobyzombie@gmail.com>
2023-12-14 04:01:11 +0100

    updated: rework of state and mongodb

--- components/ChatBot.tsx
+++ components/ChatBot.tsx
@@ -3,53 +3,110 @@
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Sigma } from "lucide-react";
-import { User } from "lucide-react";
-import { useMutation } from "@tanstack/react-query";
-import { sendPrompt } from "@/utils/sendPrompt";
+import { Sigma, SearchCheck } from "lucide-react";
+import { Bot } from "lucide-react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getAIHistory, sendPrompt, sendQuestionAnswers } from "@/utils/sendPrompt";
 import { PromptResponse, ResponseBoxProps } from "@/lib/types";
 
 function ResponseBox({ response }: ResponseBoxProps) {
-	return (
-		<div className="flex py-4 border-b place-content-start border-gray-500">
-			<div className="py-2 px-4">
-				<User size={24} />
-			</div>
-			<p className="flex-1 break-words overflow-hidden">{response}</p>
-		</div>
-	);
+    const sections = response.ai_response.split(/(Question \d+:|question \d+:|Question:|Explanation:|Correction:|Additional Exercise:)/).filter(Boolean);
+
+    return (
+        <div className="flex py-4 border-b place-content-start border-gray-500">
+            <div className="px-4">
+                <Bot size={24} />
+            </div>
+            <div className="flex-1">
+                {sections.map((section, index) => (
+					<>
+						<p key={index} className="break-words overflow-hidden">
+							{section}
+						</p>
+						<br />
+					</>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default function Chatbot() {
+	const queryClient = useQueryClient();
+	const { data, isLoading, error } = useQuery({ queryKey: ['history'], queryFn: () => getAIHistory() });
 	const [input, setInput] = useState<string>("");
-	const [responses, setResponses] = useState<PromptResponse[]>([]);	// Because I got no db rn bruh
 
-	const mutationPrompt = useMutation({
-		mutationKey: ["prompt"],
+
+	// Can put into custom hooks after
+	const mutationKnowledgePrompt = useMutation({
+		mutationKey: ["initial"],
 		mutationFn: (promptData: string) => sendPrompt(promptData),
-		onSuccess: (data) => {
-			setResponses(currentResponses => [...currentResponses, data]);
+		onSuccess: () => {
+			queryClient.invalidateQueries({queryKey: ['history']});
 		},
 		onError: (error) => {
 			console.log("Error: ", error);
 		},
 	});
+
+	const mutationCheckingPrompt = useMutation({
+		mutationKey: ["prompt"],
+		mutationFn: (promptData: string) => sendQuestionAnswers(promptData),
+		onSuccess: () => {
+			queryClient.invalidateQueries({queryKey: ['history']});
+		},
+		onError: (error) => {
+			console.log("Error: ", error);
+		},
+	});
+
 	
-	const handleSubmit = () => {
+	const handleContextSubmit = () => {
 		if (input.trim()) {
-			mutationPrompt.mutate(input);
+			mutationKnowledgePrompt.mutate(input);
 			setInput("");
 		}
 	};
 
+	const handleAnswerSubmit = () => {
+		if (input.trim()) {
+			mutationCheckingPrompt.mutate(input);
+			setInput("");
+		}
+	}
+
+	if (isLoading) {
+		return (
+			<div>
+                Loading...
+            </div>
+		);
+	}
+
+    if (error) {
+        return (
+            <div>
+                Error: {error.message}
+            </div>
+        );
+    }
+
+    if (!data) {
+        return (
+            <div>
+                Error: No data found
+            </div>
+        );
+    }
+
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between py-12">
 			<div className="flex flex-col md:w-1/2 w-full">
-				{responses.map((response, index) => (
-					<ResponseBox key={index} response={response.response} />
+				{data.map((response, index) => (
+					<ResponseBox key={index} response={response} />
 				))}
 			</div>
-			<div className="flex md:w-1/2 w-full gap-2">
+			<div className="flex md:w-1/2 w-full gap-2 pt-12">
 				<Input 
 					className="border-gray-500 bg-transparent flex-1" 
 					value={input} 
@@ -57,9 +114,15 @@ export default function Chatbot() {
 				/>
 				<Button
 					className="bg-gray-500 hover:bg-gray-400"
-					onClick={handleSubmit}
+					onClick={handleContextSubmit}
 				>
 					<Sigma size={24} />
+				</Button>
+				<Button
+					className="bg-gray-500 hover:bg-gray-400"
+					onClick={handleAnswerSubmit}
+				>
+					<SearchCheck size={24} />
 				</Button>
 			</div>
 		</main>
