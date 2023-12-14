ab152909d9c6150b4df8e8cdd57d138a5f2db8b2
R3call <thenoobyzombie@gmail.com>
2023-12-14 04:01:38 +0100

    updated: types for api

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,18 @@
 export interface PromptResponse {
+    _id: string;
     response: string;
 }
 
+export interface AIResponse {
+    ai_response: string;
+}
+
+export type AIResponseHistory = AIResponse[];
 
 export interface InputPromptsProps {
 	message: string;
 }
 
 export interface ResponseBoxProps {
-	response: string;
+	response: AIResponse;
 }
